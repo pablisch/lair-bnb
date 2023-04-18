d3e316e74ebc5fa44298e992e0cc8593e949ae52
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-04-18 14:30:05 +0100

    Added the DB connection edit to the test

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require_relative 'lib/space_repo'
 require_relative 'lib/database_connection'
 
-DatabaseConnection.connect('makersbnb')
+DatabaseConnection.connect('makersbnb') unless ENV['ENV'] == 'test'
 
 class Application < Sinatra::Base
   configure :development do
