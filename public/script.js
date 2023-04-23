2d0679adb92b8e822830f3288eea93b7e52ee224
Pablo <pablito@me.com>
2023-04-23 20:37:27 +0100

    minor colour changes

--- public/script.js
+++ public/script.js
@@ -25,7 +25,7 @@ function fade() {
   title.style.opacity = '0.1';
   title.style.transition = 'opacity 3s ease';
   secret.style.color = '#ffc400';
-  secret.style.transition = 'color 10s ease';
+  secret.style.transition = 'color 7s ease';
   black_ring.style.opacity = '1';
   black_ring.style.transition = 'opacity 3s ease';
   red_ring.style.opacity = '1';
@@ -34,8 +34,8 @@ function fade() {
   yellow_ring.style.transition = 'opacity 9s ease';
   nav.style.background = '#000';
   nav.style.transition = 'background 10s ease';
-  body.style.background = '#242424';
-  body.style.transition = 'background 10s ease';
+  body.style.background = '#0b0b0b';
+  body.style.transition = 'background 7s ease';
   spaces.style.opacity = '0';
   spaces.style.transition = 'opacity 3s ease';
   dates.style.opacity = '0';
@@ -49,7 +49,7 @@ function opaque() {
   secret.style.color = '#f5f4f4';
   secret.style.transition = 'color 1s ease';
   black_ring.style.opacity = '0';
-  black_ring.style.transition = 'opacity 3s ease';
+  black_ring.style.transition = 'opacity 9s ease';
   red_ring.style.opacity = '0';
   red_ring.style.transition = 'opacity 8s ease';
   yellow_ring.style.opacity = '0';
@@ -57,7 +57,7 @@ function opaque() {
   nav.style.background = '#fff';
   nav.style.transition = 'background 3s ease';
   body.style.background = '#f5f4f4';
-  body.style.transition = 'background 3s ease';
+  body.style.transition = 'background 10s ease';
   spaces.style.opacity = '1';
   spaces.style.transition = 'opacity 2s ease';
   dates.style.opacity = '1';
